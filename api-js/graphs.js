1136762410545e9cdddbf57f00b5e95ed6eb8d0e
Vishwanath Mandge (Consultant) <Vishwanath.Mandge@in.ril.com>
2018-09-03 18:32:14 +0530

    added graphs & trip details page

--- api-js/graphs.js
+++ api-js/graphs.js
@@ -1,16 +1,17 @@
-/* $.ajax({
+/*
+ $.ajax({
         asyn: true,
         crossDomain: true,
-        url: "http://10.26.32.4/api/rating-service/rate/ridecount/week",
+        url: "http://10.26.32.4/api/rating-service/rate/ridecount/days",
         dataType: 'JSON',
         callback: 'callback',
-        type: 'GET',
+        type: 'POST',
         success: function(result) {
             var chartdata = jQuery.parseJSON(JSON.stringify(result))
             console.log(result)
         }
-    });*/
-
+    });
+*/
 AmCharts.makeChart("rides", {
     "type": "serial",
     "hideCredits": true,
@@ -101,6 +102,116 @@ AmCharts.makeChart("rides", {
         "gridAlpha": 0,
         "tickPosition": "start",
         "tickLength": 20,
-        "labelRotation": 45
+        "labelRotation": 80
     }
+});
+
+$(document).ready(function () {   
+    $(function () {
+        $("[id*=btnok]").click(function () {
+            
+
+
+                var obj = {};
+                obj = $.trim($("[id*=nodays]").val());
+                $.ajax({
+                    type: "POST",
+                    "headers": {
+                        "content-type": "application/json",
+                        "cache-control": "no-cache"
+                    },
+                    url: "http://10.26.32.4/api/rating-service/rate/ridecount/days/",
+                    data: JSON.stringify(obj),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (result) {    
+                        console.log(result)
+                        var chart = AmCharts.makeChart("rides", {
+                            "type": "serial",
+                            "hideCredits": true,
+                            "fixedColumnWidth": '10px',
+                            "dataProvider": result,
+                            "legend": {
+                                "horizontalGap": 10,
+                                "maxColumns": 1,
+                                "position": "right",
+                                "useGraphSettings": true,
+                                "markerSize": 10
+                            },
+                            "valueAxes": [{
+                                "gridColor": "#FFFFFF",
+                                "gridAlpha": 0.2,
+                                "dashLength": 0
+                            }],
+                            "gridAboveGraphs": true,
+                            "startDuration": 1,
+                            "valueAxes": [{
+                                "stackType": "regular",
+                                "axisAlpha": 0.3,
+                                "gridAlpha": 0,
+                                "minimum": 0,
+                                "maximum": 50,
+                                "gridCount": 1
+
+                            }],
+                            "graphs": [{
+                                "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+                                "fillColors": "#47b012",
+                                "lineColor": "#47b012",
+                                "fillAlphas": 0.8,
+                                "labelText": "[[value]]",
+                                "lineAlpha": 0.3,
+                                "title": "Completed Rides",
+                                "type": "column",
+                                "color": "#000000",
+                                "valueField": "completedTrip",
+                                "fixedColumnWidth": 25
+                            }, {
+                                "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+                                "fillColors": "#fff138",
+                                "lineColor": "#fff138",
+                                "fillAlphas": 0.8,
+                                "labelText": "[[value]]",
+                                "lineAlpha": 0.3,
+                                "title": "Not Ended",
+                                "type": "column",
+                                "color": "#000000",
+                                "valueField": "activeTrip",
+                                "fixedColumnWidth": 25
+                            }, {
+                                "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+                                "fillColors": "#dd111b",
+                                "lineColor": "#dd111b",
+                                "fillAlphas": 0.8,
+                                "labelText": "[[value]]",
+                                "lineAlpha": 0.3,
+                                "title": "Canceled Rides",
+                                "type": "column",
+                                "color": "#000000",
+                                "valueField": "cancelledTrip",
+                                "fixedColumnWidth": 25
+                            }],
+                            "chartCursor": {
+                                "categoryBalloonEnabled": false,
+                                "cursorAlpha": 0,
+                                "zoomable": false
+                            },
+                            "categoryField": "creationDate",
+                            "categoryAxis": {
+                                "gridPosition": "start",
+                                "gridAlpha": 0,
+                                "tickPosition": "start",
+                                "tickLength": 20,
+                                "labelRotation": 80
+                            }
+                        });
+                    },
+                });
+                return false;
+            
+        });
+
+
+
+    });
 });
